9c8edb900191b832416afd77e02dbb19f29a4f20
josuecb <josuecb@monopolio.io>
2024-06-17 16:36:54 -0400

    modal edit create product

--- src/modules/nomenclators/products/index.tsx
+++ src/modules/nomenclators/products/index.tsx
@@ -1,28 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { Box, Select } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Box, useDisclosure } from "@chakra-ui/react";
 import { BarFilter } from "@/frontend/core/components/BarFilter";
 import CRUDActionsButtonGroup from "./components/CRUDActionsButtonGroup";
 import CRUDTable from "./components/ProductTable";
-import { useSession } from "next-auth/react";
-import { get_bussiness } from "@/helper/requests/Bussiness";
-import { Bussines, Shop } from "@/backend/types/UserType";
+import CreateEditProductDialog from "./dialog/CreateEditProductDialog";
+import { Product } from "@/backend/types/UserType";
 
 export default function NomenclatorsProductsScreen() {
-    const { data } = useSession()
-    const [listShop, setListShop] = useState([] as Array<Shop>)
-    const [shopSelect, setShopSelect] = useState(0 as number)
-
-    useEffect(() => {
-        const Load = async () => {
-            // obteniendo todas las shops del negocio al que pertence la tienda del usuario autenticado
-            const businesses_id = data?.user.shop.businesses[0].id
-            await get_bussiness({ page: 1, pageSize: 50, filter: { "id": businesses_id } }, (status: number, data: any) => {
-                const shops = (data.data[0] as Bussines).shops
-                setListShop(shops)
-            })
-        }
-        Load()
-    }, [])
+    const [action, setAction] = useState("" as string)
+    const [productsSelects, setProductsSelects] = useState([] as Array<Product>)
+    const {
+        isOpen,
+        onOpen,
+        onClose,
+    } = useDisclosure();
 
     return (
         <Box>
@@ -42,33 +33,33 @@ export default function NomenclatorsProductsScreen() {
                 ]}
             >
 
-                <Select
-                    fontSize={'13px'}
-                    colorScheme="cyan"
-                    minWidth={"210px"}
-                    backgroundColor={"white"}
-                    value={shopSelect}
-                    onChange={t => setShopSelect(parseInt(t.target.value))}
-                >
-                    {listShop.map((shop, index) => (
-                        <option key={index} value={shop.id}>{shop.name}</option>
-                    ))}
-                </Select>
-
                 <CRUDActionsButtonGroup
-                   
+                    onCreateEdit={()=>{
+                        setProductsSelects([])
+                        setAction("create")
+                        onOpen()
+                    }}
                 />
             </BarFilter>
             {/* Fin */}
 
             {/* Tabla */}
-            <CRUDTable 
-                 shopSelect={shopSelect}
+            <CRUDTable
+                onEdit={(product: Product)=>{
+                    setProductsSelects([product])
+                    setAction("edit")
+                    onOpen()
+                }}
             />
             {/* Fin */}
 
             {/* Ventanas modales */}
-
+            <CreateEditProductDialog 
+                action={action}
+                isOpen={isOpen}
+                onClose={onClose}
+                product={productsSelects[0]}
+            />
             {/* Fin */}
         </Box>
     )
