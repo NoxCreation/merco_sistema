8f91a271cf19a382867750417416928220525e50
Leonardo Arencibia <leonardoarencibia2002@gmail.com>
2024-06-20 22:38:29 -0400

    feat(New Component): Make autocompletion select

--- src/modules/configuration/components/settings/VariablePaymentSettings.tsx
+++ src/modules/configuration/components/settings/VariablePaymentSettings.tsx
@@ -1,12 +1,5 @@
 import GenericContainer from "@/frontend/core/components/GenericContainer";
-import {
-  Stack,
-  FormControl,
-  FormLabel,
-  Flex,
-  Button,
-  Select,
-} from "@chakra-ui/react";
+import { Stack } from "@chakra-ui/react";
 import React from "react";
 import UnitsSection from "../sections/UnitsSection";
 import QuantitySection from "../sections/QuantitySection";
@@ -14,7 +7,11 @@ import FixedPaymentQuantitySection from "../sections/FixedPaymentQuantitySection
 
 export default function VariablePaymentSettings() {
   return (
-    <GenericContainer title="Reglas pagos variables" width={"full"} height={"fit-content"}>
+    <GenericContainer
+      title="Reglas pagos variables"
+      width={"full"}
+      height={"fit-content"}
+    >
       <Stack spacing={"10px"}>
         <UnitsSection />
         <QuantitySection />
