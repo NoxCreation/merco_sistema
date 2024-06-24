4847847f674dbd86e5a06c4f50570a8c958ecdea
josuecb <josuecb@monopolio.io>
2024-06-24 16:01:37 -0400

    CRUD coin

---
+++ src/modules/nomenclators/coins/dialog/CreateEditCoinDialog.tsx
@@ -0,0 +1,147 @@
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Stack,
+  Box,
+  Input,
+  useToast,
+  Checkbox,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { useGetBussiness } from "@/helper/hooks/useGetBussiness";
+import { create_edit_unit, get_unit } from "@/helper/requests/Unit";
+import { Coin } from "@/backend/types";
+import { create_edit_coin } from "@/helper/requests/Coin";
+
+interface Props {
+  action: string
+  isOpen: boolean
+  onClose: () => void
+  coin?: Coin
+}
+
+export default function CreateEditCoinDialog({
+  action,
+  isOpen,
+  onClose,
+  coin
+}: Props) {
+  const toast = useToast()
+
+  const [active, setActive] = useState(true as boolean)
+  const [value_change, setValueChange] = useState(0 as number)
+  const [symbol, setSymbol] = useState("" as string)
+
+  const businesses = useGetBussiness()
+
+  useEffect(() => {
+    if (isOpen) {
+      if (action == 'edit') {
+        setActive(coin?.active as boolean)
+        setValueChange(coin?.value_change as number)
+        setSymbol(coin?.symbol as string)
+      }
+      else {
+        setActive(true)
+        setValueChange(0)
+        setSymbol("")
+      }
+    }
+  }, [isOpen])
+
+  const isValid = () => {
+    let valid = true
+    if (
+      value_change == 0 ||
+      symbol == ""
+    ) {
+      valid = false
+    }
+
+    return valid
+  }
+
+  const onCreateEdit = async () => {
+    if (isValid()) {
+      const data = {
+        active,
+        symbol,
+        value_change,
+        businessId: businesses?.id
+      }
+      await create_edit_coin(action, coin?.id as number, data, (status: number, data: any) => {
+        if (status == 200 && (data[0] == undefined || data[0] == 1)) {
+          onClose()
+        }
+        else {
+          console.log("error", status, data)
+          toast({
+            description: "Ocurrió un error al editar/crear. Revise tenga creado y elegido al menos una unidad y una categoría.",
+            status: 'error',
+            duration: 9000,
+            isClosable: true,
+            variant: "error"
+          })
+        }
+      })
+    }
+    else {
+      toast({
+        description: "Hay campos sin llenar o con valores no válidos.",
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+        variant: "error"
+      })
+    }
+
+  }
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      isCentered
+      scrollBehavior="inside"
+    >
+      <ModalOverlay
+        bg='#00000030'
+        backdropFilter='blur(10px)'
+      />
+      <ModalContent>
+        <ModalHeader>{action == "edit" ? "Editar" : "Crear"}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Stack spacing={3}>
+            <FormControl>
+              <Checkbox isChecked={active} onChange={t => setActive(t.target.checked)}>Activo</Checkbox>
+            </FormControl>
+            <FormControl>
+              <FormLabel><Box as="span" color={"red"}>*</Box> Valor de Cambio</FormLabel>
+              <Input type="text" value={value_change} onChange={t => setValueChange(parseFloat(t.target.value))} />
+            </FormControl>
+            <FormControl>
+              <FormLabel><Box as="span" color={"red"}>*</Box> Símbolo</FormLabel>
+              <Input type="text" value={symbol} onChange={t => setSymbol(t.target.value)} />
+            </FormControl>
+          </Stack>
+        </ModalBody>
+
+        <ModalFooter gap={5} display={'flex'}>
+          <Button variant={'ghost'} onClick={onClose}>Cancelar</Button>
+          <Button onClick={onCreateEdit} color={"white"}>
+            Guardar
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
---
+++ src/modules/nomenclators/coins/index.tsx
@@ -0,0 +1,331 @@
+import React, { useEffect, useState } from "react";
+import {
+    Box,
+    Checkbox,
+    useDisclosure,
+    useToast,
+    Text,
+    Badge
+} from "@chakra-ui/react";
+import { BarFilter } from "@/frontend/core/components/BarFilter";
+import { Coin } from "@/backend/types";
+import { useGetBussiness } from "@/helper/hooks/useGetBussiness";
+import { get_coin, remove_coin } from "@/helper/requests/Coin";
+import swal from 'sweetalert';
+import { MapData } from "@/helper/maps";
+import { download_excel } from "@/helper/requests/Endpoints";
+import { ColumnDef } from "@tanstack/react-table";
+import CRUDActionsButtonGroup from "@/frontend/core/components/CRUD/CRUDActionsButtonGroup";
+import CRUDTable from "@/frontend/core/components/CRUD/CRUDTable";
+import { Loading } from "@/frontend/core/components/Loading";
+import CreateEditCoinDialog from "./dialog/CreateEditCoinDialog";
+
+export default function NomenclatorsCoinsScreen() {
+    const [action, setAction] = useState("" as string)
+    const [loading, setLoading] = useState(true)
+    const [pagination, setPagination] = useState({
+        page: 1,
+        pageSize: 10,
+        count: 10
+    } as {
+        page: number,
+        pageSize: number,
+        count: number
+    })
+    const [items, setItems] = useState([] as Array<Coin>)
+    const [itemsSelects, setItemsSelects] = useState([] as Array<Coin>)
+    const {
+        isOpen,
+        onOpen,
+        onClose,
+    } = useDisclosure();
+    const businesses = useGetBussiness()
+    const toast = useToast()
+
+    useEffect(() => {
+        onLoad(pagination.page, pagination.pageSize)
+    }, [isOpen]);
+
+    const onLoad = async (npage?: number, npageSize?: number, new_filter?: {}) => {
+        setLoading(true);
+        // Filtrar por el id del negocio
+        const filter = {
+            businessId: businesses?.id,
+            ...new_filter
+        }
+        await get_coin({ page: npage ? npage : pagination.page, pageSize: npageSize ? npageSize : pagination.pageSize, filter }, (status: number, data: any) => {
+            if (status == 200) {
+                setItems(data.data)
+                let temp = JSON.parse(JSON.stringify(pagination))
+                temp.count = data.count
+                temp.page = data.page
+                setPagination(temp)
+            }
+            else {
+                console.log("error", status, data)
+                toast({
+                    description: "Ah ocurrido un error al intentar cargar los productos",
+                    status: 'error',
+                    duration: 9000,
+                    isClosable: true,
+                    variant: "error"
+                })
+            }
+            setLoading(false)
+        })
+    }
+
+    const onRemove = async (charge: Coin) => {
+        swal({
+            title: "¿Está seguro?",
+            text: "Si elimina el registro no podrá recuperarlo, ¿está seguro de querer continuar?",
+            icon: "warning",
+            buttons: ["Cancelar", "Eliminar"],
+            dangerMode: true,
+        })
+            .then((willDelete) => {
+                if (willDelete) {
+                    remove_coin(charge.id, (status: number, data: any) => {
+                        if (status == 200 && data == 1) {
+                            onLoad(pagination.page, pagination.pageSize)
+                            swal("¡Se ha eliminado satisfactoriamente!", {
+                                icon: "success",
+                            });
+                        }
+                        else {
+                            console.log("error", status, data)
+                            toast({
+                                description: "No se ha podido eliminar el elemento.",
+                                status: 'error',
+                                duration: 9000,
+                                isClosable: true,
+                                variant: "error"
+                            })
+                        }
+                    })
+                }
+            });
+    }
+
+    const onMultipleRemove = async () => {
+        if (itemsSelects.length > 0)
+            swal({
+                title: "¿Está seguro?",
+                text: "Si elimina los registros seleccionados no podrá recuperarlos, ¿está seguro de querer continuar?",
+                icon: "warning",
+                buttons: ["Cancelar", "Eliminar"],
+                dangerMode: true,
+            })
+                .then(async (willDelete) => {
+                    if (willDelete) {
+                        let flag = false
+                        for (let i = 0; i < itemsSelects.length; i++) {
+                            remove_coin(itemsSelects[i].id, (status: number, data: any) => {
+                                onLoad(pagination.page, pagination.pageSize)
+                                if (data != 1 || status != 200) {
+                                    console.log("error", status, data)
+                                    flag = true
+                                }
+                            })
+                        }
+                        if (flag)
+                            toast({
+                                description: "Al menos un elemento no pudo ser eliminado.",
+                                status: 'error',
+                                duration: 9000,
+                                isClosable: true,
+                                variant: "error"
+                            })
+                        else
+                            swal("¡Se ha eliminado satisfactoriamente!", {
+                                icon: "success",
+                            });
+                    }
+                });
+    }
+
+    const onEdit = (Employees: Coin) => {
+        setItemsSelects([Employees])
+        setAction("edit")
+        onOpen()
+    }
+
+    const onFind = (column: string, value: string) => {
+        let temp = JSON.parse(JSON.stringify(pagination))
+        temp.page = 1
+        setPagination(temp)
+        if (value != "") {
+            if (column == "categoryId") {
+                onLoad(1, pagination.pageSize, {
+                    relations: {
+                        category: {
+                            name: {
+                                LIKE: value
+                            }
+                        }
+                    }
+                })
+            }
+            else
+                onLoad(1, pagination.pageSize, {
+                    [column]: {
+                        LIKE: value
+                    }
+                })
+        }
+        else
+            onLoad(1, pagination.pageSize)
+    }
+
+    const onDownloadExcel = () => {
+        let columns = {
+            "first_name": "Nombres",
+            "last_name": "Apellidos",
+            "ci": "Carnet de Identidad",
+            "email": "Correo",
+            "phone": "Teléfono",
+            "chargeemployee.name": "Cargo"
+        };
+        let row = MapData(itemsSelects.length > 0 ? itemsSelects : items, columns)
+        download_excel(columns, row)
+    }
+
+    const columns: ColumnDef<Coin>[] = [
+        {
+            header: ({ table }) => (
+                <Checkbox
+                    size={'sm'}
+                    colorScheme="cyan"
+                    isChecked={table.getIsAllRowsSelected()}
+                    isIndeterminate={table.getIsSomeRowsSelected()}
+                    onChange={(event) => {
+                        table.toggleAllRowsSelected(event.target.checked);
+                    }}
+                >
+                    <Text fontSize={'12px'}>Id</Text>
+                </Checkbox>
+            ),
+            accessorKey: "id",
+            /* id: "id", */
+            cell: ({ row, getValue, cell }) => {
+                const canRemove = cell.row.original.canRemove
+                if (canRemove)
+                    return (
+                        <Checkbox
+                            size={'sm'}
+                            colorScheme="cyan"
+                            type="checkbox"
+                            isChecked={row.getIsSelected()}
+                            onChange={(event) => row.toggleSelected(event.target.checked)}
+                            fontSize={'0.75rem'}
+                        >
+                            {getValue<string>()}
+                        </Checkbox>
+                    )
+                else
+                    return (
+                        <>
+                            {getValue<string>()}
+                        </>
+                    )
+            },
+        },
+        {
+            header: "Activa",
+            /* id: "active", */
+            accessorKey: "active",
+            cell: ({ getValue }) => {
+                if (getValue<boolean>() == true)
+                    return <Badge variant='solid' colorScheme='green' fontSize={'9px'}>Activa</Badge>
+                else if (getValue<boolean>() == false)
+                        return <Badge variant='solid' colorScheme='red' fontSize={'9px'}>Inactiva</Badge>
+            }
+        },
+        {
+            header: "Símbolo",
+            accessorKey: "symbol",
+            id: "symbol"
+        },
+        {
+            header: "Valor de Cambio",
+            accessorKey: "value_change",
+            id: "value_change"
+        },
+        {
+            id: "actions",
+            cell: (props) => {
+                const canRemove = props.cell.row.original.canRemove
+                if (canRemove)
+                    return (
+                        <CRUDActionsButtonGroup inTable onCreateEdit={() => {
+                            onEdit(items[props.row.index])
+                        }} onRemove={() => {
+                            onRemove(items[props.row.index])
+                        }} />
+                    )
+            }
+        },
+    ];
+
+    return (
+        <Box>
+            <Loading isLoading={loading} />
+
+            {/* Barra de Filteros */}
+            <BarFilter
+                breadcrumb={[
+                    {
+                        label: 'Nomencladores',
+                        icon: undefined,
+                        link: '/nomenclators/coins'
+                    },
+                    {
+                        label: 'Monedas',
+                        icon: undefined,
+                        link: '/nomenclators/coins'
+                    },
+                ]}
+            >
+                <CRUDActionsButtonGroup
+                    onCreateEdit={() => {
+                        setItemsSelects([])
+                        setAction("create")
+                        onOpen()
+                    }}
+                    onRemove={() => {
+                        onMultipleRemove()
+                    }}
+                    column_find="first_name"
+                    onFind={onFind}
+                />
+            </BarFilter>
+            {/* Fin */}
+
+            {/* Tabla */}
+            <CRUDTable
+                onSelectItems={(products: Array<Coin>) => {
+                    if (!isOpen)
+                        setItemsSelects(products)
+                }}
+                onFilter={onLoad}
+                title={"Monedas"}
+                columns={columns}
+                rows={items}
+                pagination={pagination}
+                setPagination={setPagination}
+                onFind={onFind}
+                onDownloadExcel={onDownloadExcel}
+            />
+            {/* Fin */}
+
+            {/* Ventanas modales */}
+            <CreateEditCoinDialog
+                action={action}
+                isOpen={isOpen}
+                onClose={onClose}
+                coin={itemsSelects[0]}
+            />
+            {/* Fin */}
+        </Box>
+    )
+}
